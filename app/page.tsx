16d2a8a8002be4e32bf0b2d807df5ed1be861719
Amartya Sinha <amartua2002@gmail.com>
2024-04-15 08:46:42 +0530

    Added Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import TopBanner from "@/components/banner/TopBanner";
 import CTABanner from "@/components/ctaBanner/CTABanner";
 import Header from "@/components/header/Header";
 import Hero from "@/components/hero/Hero";
+import SecondSection from "@/components/second-section/SecondSection";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <CTABanner />
       <Header />
       <Hero />
+      <SecondSection/>
     </>
   );
 }
---
+++ components/second-section/SecondSection.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+export default function SecondSection() {
+  return (
+    <div className="bg-white text-black h-[100vh]">
+      <div className="text-center mt-36">
+        <p className="text-red-400">Results focused, Customer centered</p>
+        <h1 className="font-bold text-4xl">
+          We've Been Here from the Beginning
+        </h1>
+        <p className="max-w-lg mx-auto">
+          Our more than 120 years of pest control experience began in 1901. In
+          that time, we've become an industry leader in specialized protection
+          against common pests including insects, termites, rodents, and more.
+          Our experience ensures your peace of mind.
+        </p>
+      </div>
+
+      <div className="mt-36">
+        <div className="flex px-20 bg-gray-100 ">
+          <div className="-mt-10">
+            <img src="https://picsum.photos/600/700" alt="" />
+          </div>
+          <div className="flex flex-col justify-center items-center w-full ">
+            <div>
+            <p className="text-red-400 text-center">Results focused, Customer centered</p>
+        <h1 className="font-bold text-4xl">
+          We've Been Here from the Beginning
+        </h1>
+        <p className="max-w-lg mx-auto">
+          Our more than 120 years of pest control experience began in 1901. In
+          that time, we've become an industry leader in specialized protection
+          against common pests including insects, termites, rodents, and more.
+          Our experience ensures your peace of mind.
+        </p>
+            </div>
+<div>
+    cards
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
